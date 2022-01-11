badccf4cfd622f58f9015de2f6c69c35905017b1
diego <diego_fcb_96@hotmail.com>
2022-01-10 20:34:15 -0500

    carrito de compras

---
+++ DSL/nodeDSL.groovy
@@ -0,0 +1,37 @@
+job('Aplicación de carrito de compras') {
+    description('Aplicacion de carrito de compras')
+    scm {
+        git('https://github.com/diegoCruzNe/citas1', 'main') { node ->
+            node / gitConfigName('diegocruzNe')
+            node / gitConfigEmail('diego.fcb.96@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
